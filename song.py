dcf88ed014455051967a79f800e186fe7360f89b
M1959 <mljamtracks106@gmail.com>
2025-01-01 12:53:19 -0500

    Add files via upload

---
+++ song.py
@@ -0,0 +1,10 @@
+def display_song():
+    print("The Warriorknight's Quest - A Python Script")
+    print("By Warriorknight3299\n")
+    print("[C] At sixty-five, he hung up his tie,")
+    print("[G] Dreams of glory lit his sky.")
+    print("[Am] No gold watch could define his worth,")
+    print("[F] He sought the title he’d chased since birth.")
+
+if __name__ == "__main__":
+    display_song()
